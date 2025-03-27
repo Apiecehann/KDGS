d4024b1b1ae03d0b58118775ee1e614c7b4dfa1e
nortonii <bianhao721@gmail.com>
2025-03-27 19:11:30 +0800

    mesh use cpu

--- runGs.sh
+++ runGs.sh
@@ -1,5 +1,5 @@
 pip install ./submodules/diff-gaussian-rasterization
 python train.py --eval -s /root/autodl-tmp/data/DTU/scan24 --lod 0 --voxel_size 0.001 --update_init_factor 16 --appearance_dim 0 --ratio 1  -m ./eval/scan24 --mesh -r 2
 python render.py -s /root/autodl-tmp/data/DTU/scan24 -m ./eval/scan24 -r 2
-python mesh_extract.py -s /root/autodl-tmp/data/DTU/scan24 -m ./eval/scan24 --appearance_dim 0
+python mesh_extract.py -s /root/autodl-tmp/data/DTU/scan24 -m ./eval/scan24 --appearance_dim 0 --data_device cpu
 python evaluate_dtu_mesh.py -s /root/autodl-tmp/data/DTU/scan24 -m ./eval/scan24 --DTU /root/autodl-tmp/data/DTU-origin
